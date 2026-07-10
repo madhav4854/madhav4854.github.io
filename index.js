c41afcf72ac5fa9bc42e13eb345f5a97543a0996
Madhav <madhavpenjarla.2008@gmail.com>
2026-07-10 10:55:09 +0530

    adding about file

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ btnToggle.addEventListener("click", function () {
 });
 var btnrandom = document.getElementById("btn-random-user");
 btnrandom.addEventListener("click", function () {
-
     fetch("https://randomuser.me/api/")
         .then(function(data){
             return data.json();
         })
+        
         .then(function(jsonResponse){
             var curUserObject = jsonResponse.results[0];
                 userImage.src = curUserObject.picture.large;
